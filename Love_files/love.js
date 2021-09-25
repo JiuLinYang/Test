1c8202e85d2ac4814862382e7403022442f068a4
tomas <tomas@gamstudio.net>
2021-09-26 05:00:13 +0800

    修改文本

--- Love_files/love.js
+++ Love_files/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("click here", 23, 16);
+            ctx.fillText("快点我", 40, 40);
             ctx.restore();
         },
         clear: function() {
